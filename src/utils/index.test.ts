f1eff9d4ec64bb391933b5dae42ccef2a9e8db43
oluwadamilare olusakin <oluwadamilareo@fixus.com.ng>
2021-04-29 11:54:42 +0100

    Add utils

---
+++ src/utils/index.test.ts
@@ -0,0 +1,149 @@
+import { TemperatureUnit } from "types";
+import * as Utils from "./index";
+
+describe("Utils", () => {
+  const RealDate = global.Date;
+
+  const mockDate = (customDate?: number) => {
+    global.Date = class extends RealDate {
+      constructor(date: number = 1619638584985) {
+        super();
+        if (customDate) return new RealDate(customDate);
+        return new RealDate(date);
+      }
+    } as any;
+  };
+
+  beforeEach(() => {
+    mockDate();
+  });
+
+  afterEach(() => {
+    global.Date = RealDate;
+  });
+
+  describe("convertEpochToDate", () => {
+    it("returns today if the day of year is the same", () => {
+      const today = new Date();
+      const result = Utils.convertEpochToDate(1619638584985 / 1000);
+
+      expect(result).toEqual("Today");
+    });
+
+    it("returns today if the tomorrow of year is the same as tomorrow", () => {
+      const tomorrow = new Date();
+
+      // add 1 day to today
+      tomorrow.setDate(new Date().getDate() + 1);
+
+      const result = Utils.convertEpochToDate(
+        Math.round(tomorrow.getTime() / 1000)
+      );
+
+      expect(result).toEqual("Tomorrow");
+    });
+  });
+
+  describe("outOfRange", () => {
+    it("returns true when a card is out of range to the left", () => {
+      const results = Utils.outOfRange(2, 3, 9, 3);
+
+      expect(results).toBe(true);
+    });
+
+    it("returns true when a card is out of range to the right", () => {
+      const results = Utils.outOfRange(7, 3, 9, 3);
+
+      expect(results).toBe(true);
+    });
+
+    it("returns false when there is space for the last lone element in the current view", () => {
+      const results = Utils.outOfRange(8, 6, 8, 3);
+
+      expect(results).toBe(false);
+    });
+  });
+
+  describe("convert temperature", () => {
+    it("returns celcius when unit is celcius", () => {
+      const results = Utils.convertTemperature(TemperatureUnit.C, 200);
+
+      expect(results).toBe("93.33°C");
+    });
+
+    it("returns fahernheit as a default", () => {
+      const results = Utils.convertTemperature(TemperatureUnit.F, 200);
+
+      expect(results).toBe("200°F");
+    });
+  });
+
+  describe("convert temperature", () => {
+    it("returns celcius when unit is celcius", () => {
+      const groupedData = {
+        1: [
+          {
+            arg: "4:00:00",
+            val: 283.34,
+          },
+          {
+            arg: "5:00:00",
+            val: 283.12,
+          },
+        ],
+      };
+
+      const data = [
+        {
+          dt: 1619665200,
+          temp: 283.34,
+          feels_like: 282.58,
+          pressure: 1005,
+          humidity: 83,
+          dew_point: 280.59,
+          uvi: 0,
+          clouds: 98,
+          visibility: 10000,
+          wind_speed: 3.91,
+          wind_deg: 249,
+          wind_gust: 11.35,
+          weather: [
+            {
+              id: 804,
+              main: "Clouds",
+              description: "overcast clouds",
+              icon: "04n",
+            },
+          ],
+          pop: 0,
+        },
+        {
+          dt: 1619668800,
+          temp: 283.12,
+          feels_like: 280.91,
+          pressure: 1005,
+          humidity: 85,
+          dew_point: 280.72,
+          uvi: 0,
+          clouds: 99,
+          visibility: 10000,
+          wind_speed: 4.47,
+          wind_deg: 256,
+          wind_gust: 11.7,
+          weather: [
+            {
+              id: 804,
+              main: "Clouds",
+              description: "overcast clouds",
+              icon: "04d",
+            },
+          ],
+          pop: 0,
+        },
+      ];
+      const results = Utils.groupHourlyDataByDay(data);
+
+      expect(results).toEqual(groupedData);
+    });
+  });
+});
---
+++ src/utils/index.ts
@@ -0,0 +1,122 @@
+import { TemperatureTimePair, TemperatureUnit } from "types";
+import moment from "moment";
+
+export const isToday = (epochTime: number) => {
+  const today = new Date();
+  const date = new Date(epochTime * 1000);
+  return (
+    today.getFullYear() === date.getFullYear() &&
+    today.getMonth() === date.getMonth() &&
+    today.getDate() === date.getDate()
+  );
+};
+
+export const isTomorrow = (epochTime: number) => {
+  const today = new Date();
+  const date = new Date(epochTime * 1000);
+  return (
+    today.getFullYear() === date.getFullYear() &&
+    today.getMonth() === date.getMonth() &&
+    today.getDate() + 1 === date.getDate()
+  );
+};
+
+export const convertEpochToDate = (epochTime: number) => {
+  if (isToday(epochTime)) return "Today";
+  if (isTomorrow(epochTime)) return "Tomorrow";
+  return moment(epochTime * 1000).format("MMM Do YY");
+};
+
+export const getResponsiveCardWidths = (
+  phoneScreens: boolean,
+  tabletScreens: boolean
+) => {
+  if (phoneScreens) return "100%";
+  if (tabletScreens) return "47%";
+};
+
+export const outOfRange = (
+  currentIndex: number,
+  currentStartIndex: number,
+  sizeOfData: number,
+  upperLimit: number
+) => {
+  if (
+    sizeOfData % upperLimit !== 0 &&
+    // moving forward will lead us to the last data card
+    currentStartIndex + (sizeOfData % upperLimit) === sizeOfData &&
+    // the next card is the last one or the two before the last
+    (currentIndex + 1 === sizeOfData ||
+      sizeOfData - currentIndex < upperLimit + 1)
+  )
+    return false;
+  if (currentIndex - currentStartIndex > upperLimit - 1) return true;
+  if (currentIndex - currentStartIndex < 0) return true;
+};
+
+export const convertTemperature = (
+  unit: TemperatureUnit,
+  temperature: number
+) => {
+  if (unit === TemperatureUnit.F) return `${temperature}°F`;
+
+  const newTemperature = ((temperature - 32) * (5 / 9)).toFixed(2);
+
+  return `${newTemperature}°C`;
+};
+
+export const convertTemperatureRaw = (
+  unit: TemperatureUnit,
+  temperature: number
+): number => {
+  if (unit === TemperatureUnit.F) return temperature;
+
+  const newTemperature = ((temperature - 32) * (5 / 9)).toFixed(2);
+
+  return parseInt(newTemperature);
+};
+
+export const dayOfYear = (date: number) => {
+  return moment(date * 1000).dayOfYear();
+};
+
+export const dateToday = (epochTime: number) => {
+  return moment(epochTime * 1000);
+};
+
+export const formatDate = (epochTime: number, format: string) => {
+  return dateToday(epochTime).format(format);
+};
+
+export const groupHourlyDataByDay = (
+  data: { temp: number; dt: number }[]
+): TemperatureTimePair => {
+  let currentDay = dayOfYear(data[0].dt);
+  const groupedDataHash: { [key: number]: { arg: string; val: number }[] } = {};
+
+  data.forEach((hour) => {
+    if (currentDay === dayOfYear(hour.dt)) {
+      if (groupedDataHash[currentDay]) {
+        groupedDataHash[currentDay].push({
+          arg: formatDate(hour.dt, "h:mm:ss"),
+          val: hour.temp,
+        });
+      } else {
+        groupedDataHash[currentDay] = [];
+        groupedDataHash[currentDay].push({
+          arg: formatDate(hour.dt, "h:mm:ss"),
+          val: hour.temp,
+        });
+      }
+    } else {
+      currentDay = dayOfYear(hour.dt);
+      groupedDataHash[currentDay] = [];
+      groupedDataHash[currentDay].push({
+        arg: formatDate(hour.dt, "h:mm:ss"),
+        val: hour.temp,
+      });
+    }
+  });
+
+  return groupedDataHash;
+};
