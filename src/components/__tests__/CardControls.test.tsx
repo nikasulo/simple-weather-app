e3e95ad52040889a918ccdf4fabccc00fc7fb582
oluwadamilare olusakin <oluwadamilareo@fixus.com.ng>
2021-04-29 11:54:08 +0100

    Add card controls

---
+++ src/components/__tests__/CardControls.test.tsx
@@ -0,0 +1,139 @@
+import userEvent from "@testing-library/user-event";
+
+import { render, screen } from "test/utils";
+import CardControls from "components/CardControls";
+
+describe("Card controls", () => {
+  it("renders without crashing", () => {
+    const handleRightArrow = jest.fn();
+    const handleLeftArrow = jest.fn();
+    render(
+      <CardControls
+        upperLimit={3}
+        dataSetSize={9}
+        currentStartIndex={0}
+        handleRightArrow={handleRightArrow}
+        handleLeftArrow={handleLeftArrow}
+      />,
+      {}
+    );
+  });
+
+  describe("arrow buttons", () => {
+    it("hides right arrow if there is no weather cards to its left", async () => {
+      const handleRightArrow = jest.fn();
+      const handleLeftArrow = jest.fn();
+      render(
+        <CardControls
+          upperLimit={3}
+          dataSetSize={9}
+          currentStartIndex={6}
+          handleRightArrow={handleRightArrow}
+          handleLeftArrow={handleLeftArrow}
+        />,
+        {}
+      );
+
+      const rigthArrow = screen.queryByTestId("right-arrow");
+
+      expect(rigthArrow).toBe(null);
+    });
+
+    it("hides left arrow if there is no weather cards to its right", () => {
+      const handleRightArrow = jest.fn();
+      const handleLeftArrow = jest.fn();
+      render(
+        <CardControls
+          upperLimit={3}
+          dataSetSize={9}
+          currentStartIndex={0}
+          handleRightArrow={handleRightArrow}
+          handleLeftArrow={handleLeftArrow}
+        />,
+        {}
+      );
+
+      const leftArrow = screen.queryByTestId("left-arrow");
+
+      expect(leftArrow).toBe(null);
+    });
+
+    it("shows left arrow if there are weather cards to its right", () => {
+      const handleRightArrow = jest.fn();
+      const handleLeftArrow = jest.fn();
+      render(
+        <CardControls
+          upperLimit={3}
+          dataSetSize={9}
+          currentStartIndex={3}
+          handleRightArrow={handleRightArrow}
+          handleLeftArrow={handleLeftArrow}
+        />,
+        {}
+      );
+
+      const leftArrow = screen.getByTestId("left-arrow");
+
+      expect(leftArrow).toBeDefined();
+    });
+
+    it("shows right arrow if there are weather cards to its left", () => {
+      const handleRightArrow = jest.fn();
+      const handleLeftArrow = jest.fn();
+      render(
+        <CardControls
+          upperLimit={3}
+          dataSetSize={9}
+          currentStartIndex={3}
+          handleRightArrow={handleRightArrow}
+          handleLeftArrow={handleLeftArrow}
+        />,
+        {}
+      );
+
+      const rightArrow = screen.getByTestId("right-arrow");
+
+      expect(rightArrow).toBeDefined();
+    });
+
+    it("clicking the right arrow fires the onClick handler", () => {
+      const handleRightArrow = jest.fn();
+      const handleLeftArrow = jest.fn();
+      render(
+        <CardControls
+          upperLimit={3}
+          dataSetSize={9}
+          currentStartIndex={3}
+          handleRightArrow={handleRightArrow}
+          handleLeftArrow={handleLeftArrow}
+        />,
+        {}
+      );
+
+      const rightArrow = screen.getByTestId("right-arrow");
+      const leftClick = { button: 0 };
+      userEvent.click(rightArrow, leftClick);
+      expect(handleRightArrow).toHaveBeenCalled();
+    });
+
+    it("clicking the left arrow fires the onClick handler", () => {
+      const handleRightArrow = jest.fn();
+      const handleLeftArrow = jest.fn();
+      render(
+        <CardControls
+          upperLimit={3}
+          dataSetSize={9}
+          currentStartIndex={3}
+          handleRightArrow={handleRightArrow}
+          handleLeftArrow={handleLeftArrow}
+        />,
+        {}
+      );
+
+      const leftArrow = screen.getByTestId("left-arrow");
+      const leftClick = { button: 0 };
+      userEvent.click(leftArrow, leftClick);
+      expect(handleLeftArrow).toHaveBeenCalled();
+    });
+  });
+});
