fa39de177ca11a7319add035821c258542d9d222
oluwadamilare olusakin <oluwadamilareo@fixus.com.ng>
2021-04-29 11:49:01 +0100

    Setup data fetching

---
+++ src/api/index.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { setWeatherData, setHourlyData } from "redux/actions";
+import store from "redux/store/store";
+import { groupHourlyDataByDay } from "utils";
+
+const axiosInstance = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}`,
+});
+
+export const getData = () => {
+  return axiosInstance
+    .get(`${process.env.REACT_APP_DAILY_DATA_ENDPOINT}${process.env.REACT_APP_ID}`)
+    .then(async (res) => {
+      const { daily, hourly } = res.data;
+      store.dispatch(setWeatherData(daily));
+
+      store.dispatch(setHourlyData(groupHourlyDataByDay(hourly)));
+    });
+};
